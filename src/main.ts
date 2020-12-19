ebf94926a54177b52ee593ffb16aa1e4b1380884
Daniel Marin <danielarturomt@gmail.com>
2020-12-19 17:48:56 -0300

    chore: clean up the code

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,8 @@ import * as path from 'path';
 function initialize() {
   const port = 3000;
   const app = express();
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(
-    '/node_modules',
-    express.static(path.join(__dirname, 'node_modules'))
-  );
+  app.use(express.static('src/public'));
+  app.use('/node_modules', express.static('node_modules'));
   app.listen(port, () => console.log(`Listening on port ${port}`));
 }
 
